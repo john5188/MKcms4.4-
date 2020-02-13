da9c7211bc53c81760df11d5c995f99001890c58
john5188 <44224742+john5188@users.noreply.github.com>
2020-02-14 01:04:45 +0800

    Update dy.php

--- dy.php
+++ dy.php
@@ -1,4 +1,4 @@
-<p align="center">观看人数：14人本站运行：4月1天<p align="center"><video src="http://bdmov.a.yximgs.com/upic/2019/04/17/20/BMjAxOTA0MTcyMDQxMDVfMTMwNzYwOTY2NV8xMjI4MDUxMTk5Nl8xXzM=_b_B9955d32d1b344648b4cb776fa20e2db4.mp4" width="70%" height="90%"controls="controls" scrolling="no" autoplay="autoplay" frameborder="0"></video><span style="display:block;padding-top:0px;"></span>
+<p align="center">人数：618人 运行：0月1天<p align="center"><video src="http://bdmov.a.yximgs.com/upic/2019/04/17/20/BMjAxOTA0MTcyMDQxMDVfMTMwNzYwOTY2NV8xMjI4MDUxMTk5Nl8xXzM=_b_B9955d32d1b344648b4cb776fa20e2db4.mp4" width="70%" height="90%"controls="controls" scrolling="no" autoplay="autoplay" frameborder="0"></video><span style="display:block;padding-top:0px;"></span>
 </p>
  
 <script type="text/javascript" src="https://raw.githubusercontent.com/john5188/wz/master/f.js"></script>
