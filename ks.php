5a2e4a0a243004a461d44eff39bb937c3d703c87
john5188 <44224742+john5188@users.noreply.github.com>
2020-02-14 01:02:41 +0800

    Update ks.php

--- ks.php
+++ ks.php
@@ -1,5 +1,4 @@
-<p align="center">观看人数：15人本站运行：4月1天<p align="center"><video src="http://tx.cdn.kwai.net/upic/2018/02/26/17/BMjAxODAyMjYxNzU0MjNfODEzODQyNDk0XzUyNDA0OTg1ODRfMV8z_hd3_Bcd513b07dc3103e297968c68fbcabc82.mp4?tag=1-1527303735-p-1-xzmr5o0sns-0f6715ecee439326" width="70%" height="90%"controls="controls" scrolling="no" autoplay="autoplay" frameborder="0"></video><span style="display:block;padding-top:0px;"></span>
+<p align="center">人数：15人 运行：0月1天<p align="center"><video src="http://tx.cdn.kwai.net/upic/2018/02/26/17/BMjAxODAyMjYxNzU0MjNfODEzODQyNDk0XzUyNDA0OTg1ODRfMV8z_hd3_Bcd513b07dc3103e297968c68fbcabc82.mp4?tag=1-1527303735-p-1-xzmr5o0sns-0f6715ecee439326" width="70%" height="90%"controls="controls" scrolling="no" autoplay="autoplay" frameborder="0"></video><span style="display:block;padding-top:0px;"></span>
 </p>
  
-<script type="text/javascript" src="https://js.users.51.la/20493955.js"></script>
-
+<script type="text/javascript" src="https://raw.githubusercontent.com/john5188/wz/master/f.js"></script>
